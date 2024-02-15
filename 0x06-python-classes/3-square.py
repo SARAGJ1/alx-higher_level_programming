8ab7460a9cf94c9d58c5dde322a0201e32cd2907
SARAGJ1 <sara.guerrouj.22@gmail.com>
2024-02-15 21:46:14 +0000

    hi

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,16 +5,11 @@
 class Square:
     """ Square Class"""
 
-        def __init__(self, size=0):
-            """Initialize Square with optional size"""
+    def __init__(self, size=0):
+        """Initialize Square with optional size"""
 
-            if not isinstance(size, int):
-                raise TypeError('size must be an integer')
-            elif size < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = size
-
-        def area(self):
-            """Area"""
-
-            return size*size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
