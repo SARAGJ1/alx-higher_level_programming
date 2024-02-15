5b68a230670b5aa4371b7351ad9471a4b63fd1dc
SARAGJ1 <sara.guerrouj.22@gmail.com>
2024-02-15 21:03:26 +0000

    hi

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, _Square__size):
         self._Square__size = _Square__size
 
-    def type(_Square__size):
-        if _Square__size < 0:
+    def type(self):
+        if self._Square__size < 0:
             print('size must be >= 0')
-        if not isinstance(_Square__size, int):
+        if not isinstance(self._Square__size, int):
             print('size must be an integer')
