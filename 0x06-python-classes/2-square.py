ceb1f74b1c72c02583542c3e2553af41b21bb363
SARAGJ1 <sara.guerrouj.22@gmail.com>
2024-02-15 20:45:26 +0000

    hi

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Square Module """
 
+
 class Square:
     """ Square Class"""
 
@@ -8,7 +9,7 @@ class Square:
         self._Square__size = _Square__size
 
     def checksize(self, _Square__size):
-        if _Square__size < 0 and _Suare__size != 0:
+        if _Square__size < 0 :
             print('size must be >= 0')
-        if type(_Square__size) != type(6):
+        if type(_Square__size) != int:
             print('size must be an integer')
