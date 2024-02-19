8c911c0a056354c75efe02f4e02daf465d029c6b
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-19 12:17:23 +0000

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """class MyList that inherits from list"""
 
+
 class MyList(list):
     """class MyList that inherits from list"""
 
     def __init__(self, *args):
+    """func that inherits"""
+
         super().__init__(*args)
 
     def print_sorted(self):
+    """func that organize the list"""
+
         print_sorted = sorted(self)
         print(print_sorted)
