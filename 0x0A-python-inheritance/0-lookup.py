dbf36ae54367eb7eef94b0cb4656197859bb5d13
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-19 11:30:52 +0000

    commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""list of available attributes and methods of an objec"""
+
 
 def lookup(obj):
+    """list of available attributes and methods of an objec"""
+
     return dir(obj)
