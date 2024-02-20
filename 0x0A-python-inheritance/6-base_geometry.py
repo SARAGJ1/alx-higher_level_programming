899499825efdd6351031f936d991b6938919bf47
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-20 10:59:50 +0000

    commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''not empty class.'''
+
+
+class BaseGeometry:
+        """not empty class"""
+
+        def area(self):
+            print('[Exception] {} is not implemented'.format(area(self)))
