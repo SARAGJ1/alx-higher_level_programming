cc83db080a72525b95f4b9ab6a5c195816e58822
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-20 10:45:57 +0000

    commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module for is_same_class method.'''
+
+
+def is_kind_of_class(obj, a_class):
+    """check if the object is instance of the class"""
+    
+    if isinstance(obj, a_class):
+        return True
+    return False
