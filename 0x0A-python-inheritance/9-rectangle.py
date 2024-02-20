d26d879a2aa1d9b743c74e0d097638326a2aff45
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-20 11:37:52 +0000

    commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""not empty class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class that inherit from basegeometry"""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle."""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """calculate the area"""
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return self.__width*self.__height
