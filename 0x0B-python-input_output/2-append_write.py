e17726402dc6e9a6e3bfda23263997816edd9655
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-20 23:53:01 +0000

    commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,7 +7,7 @@ def append_write(filename="", text=""):
 
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-    
+
     with open(filename, 'w', encoding="utf-8") as f:
-        text = text + read_data
-        return f.write(text)
+        append_data = text + read_data
+        return f.write(append_data)
