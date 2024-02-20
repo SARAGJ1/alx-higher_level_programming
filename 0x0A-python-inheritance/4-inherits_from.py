2fb6139c4b0011886ed3bba81c1f527411477e90
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-20 10:55:32 +0000

    commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for is_same_class method.'''
+
+def inherits_from(obj, a_class):
+    """func to ckeck if a class is inhirited from another"""
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
