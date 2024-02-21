82bf7b8a0719c9b08fd0da1517b729b242aacd99
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-21 02:38:31 +0000

    commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,6 +11,6 @@ class Student:
         self.age = age
 
     def to_json(self):
-        if type(attrs) = == list and all(type(ele) == str for ele in attrs)):
+        if type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}_
         return self.__dict__
