8b3dc3ddbda6e29ef9f4ca8c98b014bc9560cbb0
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-21 02:07:19 +0000

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def main():
-    """func main"""
+filename = "add_item.json"
 
-    filename = "add_item.json"
-
-    try:
-        my_list = load_from_json_file(filename)
-        k = my_list + sys.argv[1:]
-        save_to_json_file(k, filename)
+my_list = load_from_json_file(filename)
+k = my_list + sys.argv[1:]
+save_to_json_file(k, filename)
