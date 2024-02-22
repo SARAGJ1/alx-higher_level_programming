ae49531152b78664867e9431b4ded47e99f8f615
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-22 13:22:31 +0000

    commit

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,3 +5,15 @@
 def append_after(filename="", search_string="", new_string=""):
     """append_after"""
 
+    with open(filename, 'r', encoding="utf-8") as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.writelines(line_list)
+
