3149830822acc980a3aad5a6655a50ed3c34b9f4
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-29 19:07:48 +0000

    commit

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-from variable_load_5 import a
 
 if __name__ == "__main__":
+    with open("variable_load_5.py") as f:
+        code = compile(f.read(), "variable_load_5.py", 'exec')
+        exec(code)
     print("{}". format(a))
