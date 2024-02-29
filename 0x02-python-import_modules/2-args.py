acfa2b295acfdbd3c3602e1b4d18b2d3bc159a82
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-29 17:22:40 +0000

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ i = 1
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         print("{} argument:". format(len(sys.argv) - 1))
+    elif len(sys.argv) == 1:
+        print("{} argument.". format(len(sys.argv) - 1))
     else:
         print("{} arguments:". format(len(sys.argv) - 1))
     while i < len(sys.argv):
