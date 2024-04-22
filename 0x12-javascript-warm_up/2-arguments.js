eacc62188444e8609d422a71592953d26e8850fa
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-04-22 21:55:19 +0100

    f

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] != null)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('No argument');
+if (process.argv[2] != null && process.argv[3] == null) {
+  console.log('Argument found');
+} else if (process.argv[2] != null && process.argv[3] != null) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
