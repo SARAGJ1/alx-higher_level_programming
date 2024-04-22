ad1f0d953344217c98b482406f8dfa50bc5788a6
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-04-22 22:24:38 +0100

    h

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,9 +3,10 @@ if (process.argv[2] == null) {
   console.log('No argument');
 } else {
   const args = process.argv.slice(2);
-  let i = 0;
+  let i = 0, result = '';
   while (args[i] != null) {
-    console.log(args[i]);
+    result += args[i] + ' ';
     i = i + 1;
   }
+  console.log(result.trim());
 }
