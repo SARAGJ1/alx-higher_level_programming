f2c3ff1621d6358931f2baec6bfdf23ce055293b
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-04-23 12:06:42 +0100

    commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 function factorial (n) {
   let fac = 1;
-  if (n !== undefined) {
-    while (n !== 0) {
-      fac = fac * n;
-      n = n - 1;
-    }
-  return (fac);
-  } else {
-  return (1);
+  while (n !== 0) {
+    fac = fac * n;
+    n = n - 1;
   }
+  return (fac);
 }
-console.log(factorial(Number(process.argv[2])));
+if (process.argv[2] !== undefined) {
+  console.log(factorial(Number(process.argv[2])));
+} else {
+  console.log(1);
+}
