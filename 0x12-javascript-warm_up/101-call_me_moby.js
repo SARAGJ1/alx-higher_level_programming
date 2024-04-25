9aa85bf5591711af7d04c6d6090468c4079f2f7e
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-04-25 23:32:35 +0100

    commit

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function callMeMoby (x, theFunction) {
+  let i = 0;
+  while (i < k) {
+    theFuction();
+    i += 1;
+  }
+}
+module.exports.callMeMoby = callMeMoby;
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const k = Number(process.argv[2]);
 if (Number.isInteger(k)) {
   let result = '';
   while (i < k) {
-    result += 'x';
+    result += 'X';
     i = i + 1;
   }
   for (i = 0; i < k; i++) {
