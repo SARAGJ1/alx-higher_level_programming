b22488492aa6bf9109b7a41a322fa032ba88af9c
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-05-10 00:50:40 +0100

    commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	    echo "Usage: $0 <URL>"
-	        exit 1
-fi
-
-URL=$1
-
-BODY_SIZE=$(curl -sI $URL | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$BODY_SIZE" ]; then
-	    BODY_SIZE=$(curl -s $URL | wc -c)
-fi
-
-echo $BODY_SIZE
+#bash script 
+curl -sl "$1" | awk'/Content-Length/ {print $2}'
