d55d35abb4b1d1eb0534b323eb3794e9aa9c3a93
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-20 18:25:35 +0000

    commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that prints all integers of a list."""
+
+
+def print_list_integer(my_list=[]):
+    """function that prints all integers of a list."""
+
+    i = 0
+    while (i < len(my_list)) :
+        x = "{}".format(my_list[i])
+        print(x)
+        i = i + 1
