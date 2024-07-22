23ac386b5cf3b2cc00088b8bdbf360a5075a0bbf
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-22 17:56:03 +0000

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,7 +6,6 @@ def no_c(my_string):
     """function that removes all characters c and C from a string."""
 
     new_string = []
-         
     for char in my_string:
         if char != 'c' and char != 'C':
             new_string.append(char)
