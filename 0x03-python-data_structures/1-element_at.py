7a1082a0659d1ba321c8d9449c489f009ea70dd5
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-22 17:02:36 +0000

    commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """function that retrieves an element from a list like in C."""
 
+
 def element_at(my_list, idx):
     """function that retrieves an element from a list like in C."""
-
 
     if (idx < 0 or idx >= len(my_list)):
         return None
