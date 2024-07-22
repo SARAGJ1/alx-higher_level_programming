dbc3e1c9913113e4891a661357a19884ed1ea502
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-22 17:53:00 +0000

    commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that removes all characters c and C from a string."""
+
+
+def no_c(my_string):
+    """function that removes all characters c and C from a string."""
+
+    new_string = []
+            
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string.append(char)
+    
+    return ''.join(new_string)
+
