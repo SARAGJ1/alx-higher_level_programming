70a1dffe390fc86a4d41b4ab7e496ed609b129c6
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-24 11:05:38 +0000

    commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -10,10 +10,7 @@ def delete_at(my_list=[], idx=0):
     new_list = []
     i = 0
     while (i < len(my_list)):
-        if (i == idx):
-            i = i + 1
-        if (i >= len(my_list)):
-            return new_list
-        new_list.append(my_list[i])
-        i = i + 1
+        if i != idx:
+            new_list.append(my_list[i])
+        i += 1
     return new_list
