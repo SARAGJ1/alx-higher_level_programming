31e0719a067220e6b148e06d69d958b8cfcbd173
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-24 13:07:13 +0000

    commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+
+int is_palindrome(listint_t **head) {
+	listint_t *current;
+	listint_t *current1;
+
+	current = *head;
+	current1 = NULL;
+	while (current != NULL) {
+		listint_t *new;
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (0);
+		new->n = current->n;
+		new->next = current1;
+		current1 = new;
+		current = current->next;
+	}
+	current = *head;
+	while (current->next != NULL) {
+		if (current1->n != current->n)
+			return (0);
+		current1 = current1->next;
+		current = current->next;
+	}
+	return (1);
+}
