f6322951c29b48099cd4e3baeee7335154a6e5fe
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-24 09:33:20 +0000

    commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,7 +5,7 @@
 def multiple_returns(sentence):
     """returns tuple with the length of a string and its first character."""
 
-    if sentence is None:
+    if len(sentence) == 0:
         tuple_a = (0, None)
     else:
         tuple_a = (len(sentence), sentence[0])
