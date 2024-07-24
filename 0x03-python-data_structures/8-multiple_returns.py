ddb0acfdb16226540691003b8f0978d3aa026672
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-24 09:26:27 +0000

    commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""returns tuple with the length of a string and its first character."""
+
+
+def multiple_returns(sentence):
+    """returns tuple with the length of a string and its first character."""
+
+    tuple_a = (len(sentence), sentence[0])
+    return tuple_a
