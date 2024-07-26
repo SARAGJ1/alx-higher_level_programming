37a20fe97517e0d8d6e357ff51859fbbb018ccbf
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-26 18:17:58 +0000

    commit

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that prints a dictionary by ordered keys."""
+
+
+def print_sorted_dictionary(a_dictionary):
+    """function that prints a dictionary by ordered keys."""
+
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    for key, value in sorted_dict.items():
+        print(key, ":" , value)
