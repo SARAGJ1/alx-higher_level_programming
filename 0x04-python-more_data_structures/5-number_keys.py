4ba10ae2ee8e055859be2a6fb31c67d93ca37fc0
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-26 17:46:55 +0000

    commit

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns the number of keys in a dictionary."""
+
+
+def number_keys(a_dictionary):
+    """function that returns the number of keys in a dictionary."""
+
+    i = 0
+    for key in a_dictionary:
+        i += 1
+    return i
