aa4626153a0cd17c4e1ea33279b67e90ba3620ae
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-31 10:14:13 +0000

    commit

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -6,7 +6,5 @@ def multiply_list_map(my_list=[], number=0):
     """returns a list with all values * by a number without using any loops."""
 
     def multi(x):
-        return x * number
-    
-    y = list(map(multi, my_list))
-    return y
+        return x * number 
+    return list(map(multi, my_list))
