1b53eb30c857f95b3c9f09222beec1a851234881
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-31 12:12:00 +0000

    commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that returns the weighted average of all integers tuple"""
+
+
+def weight_average(my_list=[]):
+    """function that returns the weighted average of all integers tuple"""
+
+    r = 0
+    sum = 0
+    i = 0
+    while i < len(my_list):
+        r = r + (my_list[i][0] * my_list[i][1])
+        sum = sum + my_list[i][1]
+        i = i + 1
+    return r / sum
