3d489d2109e9d04b2a735be469b8a4cad6b01a5c
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-07-31 13:05:30 +0000

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,9 +8,11 @@ def best_score(a_dictionary):
     x = 0
     if a_dictionary == None:
         return None
+
     for value in a_dictionary.values():
         if x < value:
             x = value
+
     for key, value in a_dictionary.items():
         if value == x:
             return key
